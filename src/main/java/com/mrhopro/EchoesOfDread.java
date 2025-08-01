fbeb35fc63bd846f69c2c6270aed36961d8cecfd
Mrhopro <mrhopro611@gmail.com>
2025-08-01 14:39:45 +0300

    Initial commit

---
+++ src/main/java/com/mrhopro/EchoesOfDread.java
@@ -0,0 +1,16 @@
+package com.mrhopro;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EchoesOfDread implements ModInitializer {
+	public static final String MOD_ID = "echoes-of-dread";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
